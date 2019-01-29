c6816bc4bbdabaeff6cab40167eaffc8d8175ad9
Brooke E Kline Jr <brooke.kline@moov.io>
2019-01-29 16:29:28 -0500

    Miscellaneous Updates
    Miscellaneous Updates

---
+++ version.go
@@ -0,0 +1,7 @@
+// Copyright 2019 The Moov Authors
+// Use of this source code is governed by an Apache License
+// license that can be found in the LICENSE file.
+
+package fed
+
+const Version = "v0.1.0-dev"
